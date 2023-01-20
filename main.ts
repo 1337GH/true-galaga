c315c8624123f1300036126dc40f493f32c442a4
obryanc23 <119616236+obryanc23@users.noreply.github.com>
2023-01-20 10:19:54 -0500

    figuring out shooting and shield

--- main.ts
+++ main.ts
@@ -1,22 +1,78 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+namespace SpriteKind {
+    export const shield = SpriteKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    defense()
+    laser = false
 })
-let mySprite = sprites.create(img`
+function attack () {
+    while (true) {
+        projectile3 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . 2 . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, Ship, 0, 75)
+        pause(200)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Repeated, function () {
+    laser = true
+    attack()
+})
+function defense () {
+    shield = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 9 . . . . . . . . . . . . 9 
+        . . 9 9 . . . . . . . . . . . 9 
+        . . . 9 9 . . . . . . . . . . 9 
+        . . . . 9 9 . . . . . . . . . 9 
+        . . . . . 9 9 . . . . . . 9 9 . 
+        . . . . . . . 9 9 9 . 9 9 9 . . 
+        . . . . . . . . . 9 9 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.shield)
+    shield.follow(Ship, 75)
+}
+let shield: Sprite = null
+let projectile3: Sprite = null
+let laser = false
+let Ship: Sprite = null
+Ship = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . . . 3 . . . . . . . 
+    . . . . . . . . 3 . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 3 3 . . . . . . 
+    . . . . . . . 3 3 3 . . . . . . 
+    . . . . . . 3 3 . . 3 . . . . . 
+    . . . . 3 3 . 3 . . . 3 . . . . 
+    . . . 3 . . . 3 . . . . 3 . . . 
+    . . . . . . . 3 . . . . . 3 3 . 
+    . . . . . . . 3 . . . . . . . 3 
+    . . . . . . 3 . . . . . . . . . 
+    . . . . . . 3 . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite, 75, 0)
+controller.moveSprite(Ship, 75, 0)
