ff748eadb6d7538c3a53bde744b1860cdd3973a0
1337GH <119616372+1337GH@users.noreply.github.com>
2023-01-24 12:40:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,77 @@
 namespace SpriteKind {
     export const shield = SpriteKind.create()
+    export const shooter = SpriteKind.create()
+    export const Good = SpriteKind.create()
+    export const Bad = SpriteKind.create()
+    export const DeadZone = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.shooter, SpriteKind.DeadZone, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Good, SpriteKind.shooter, function (sprite, otherSprite) {
+    for (let index = 0; index < 3; index++) {
+        projectile22 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . 8 1 8 . . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . a 1 1 1 a . . . . . 
+            . . . . . . a 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . . a a a . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, otherSprite, -14, 100)
+        projectile22 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . 8 1 8 . . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . a 1 1 1 a . . . . . 
+            . . . . . . a 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . . a a a . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, otherSprite, 0, 100)
+        projectile22 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . 8 1 8 . . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . a 1 1 1 a . . . . . 
+            . . . . . . a 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . . a a a . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, otherSprite, 14, 100)
+        pause(100)
+    }
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.destroy()
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.shield)
     sheild_true = true
     defense()
     laser = false
@@ -14,6 +84,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         sheild_true = false
     }
 })
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.DeadZone, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeLifeBy(-1)
+})
 function attack () {
     if (laser) {
         projectile3 = sprites.createProjectileFromSprite(img`
@@ -21,34 +95,66 @@ function attack () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 2 . . . . . . . . 
-            . . . . . . . 2 . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
+            . . . . . . . . 2 . . . . . . . 
+            . . . . . . . 3 1 3 . . . . . . 
+            . . . . . . . 3 1 3 . . . . . . 
+            . . . . . . 2 1 1 1 2 . . . . . 
+            . . . . . . 2 1 1 1 2 . . . . . 
+            . . . . . . . 3 1 3 . . . . . . 
+            . . . . . . . 3 1 3 . . . . . . 
+            . . . . . . . . 2 . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, Ship, 0, -75)
+        projectile3.setKind(SpriteKind.Good)
+        projectile3.setFlag(SpriteFlag.AutoDestroy, true)
     }
 }
+sprites.onOverlap(SpriteKind.Bad, SpriteKind.Player, function (sprite, otherSprite) {
+    sprite.destroy(effects.fire, 500)
+    info.changeLifeBy(-1)
+})
+function Evil2spawn () {
+    Evil_red = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c . . . . . . . . 
+        . . . . . c c c c c . . . . . . 
+        . . . . c a a c a a c . . . . . 
+        . . . . c b a c a b c . . . . . 
+        . . . c c b b . b b c c . . . . 
+        . . . a b c . . . c b a . . . . 
+        . . . b c . . . . . c b . . . . 
+        . . . b . . . . . . . b . . . . 
+        . . . a b . . . . . b a . . . . 
+        . . . . a . . . . . a . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.shooter)
+    Evil_red.setPosition(randint(0, scene.screenWidth()), 0)
+    Evil_red.setVelocity(randint(-5, 5), 10)
+    Evil_red.setBounceOnWall(true)
+}
+sprites.onOverlap(SpriteKind.Bad, SpriteKind.shield, function (sprite, otherSprite) {
+    sprite.destroy(effects.spray, 100)
+})
 function defense () {
     shield = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . 9 9 9 9 9 9 . . . . . 
-        . . . 9 9 . . . . . . 9 9 . . . 
-        . . 9 . . . . . . . . . . 9 . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . f f 1 1 1 1 1 1 f f . . . 
+        . . f 1 1 9 9 9 9 9 9 1 1 f . . 
+        . f 1 9 9 1 1 1 1 1 1 9 9 1 f . 
+        f 1 9 1 1 f f f f f f 1 1 9 1 f 
+        f 1 1 f f . . . . . . f f 1 1 f 
+        f f f . . . . . . . . . . f f f 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -57,31 +163,99 @@ function defense () {
         `, SpriteKind.shield)
     shield.setPosition(Ship.x, Ship.y + -6)
 }
+sprites.onOverlap(SpriteKind.Good, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.destroy()
+})
+let Evil: Sprite = null
+let Evil_red: Sprite = null
 let projectile3: Sprite = null
 let shield: Sprite = null
 let laser = false
 let sheild_true = false
+let projectile22: Sprite = null
 let Ship: Sprite = null
 Ship = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . 3 . . . . . . . 
-    . . . . . . . . 3 . . . . . . . 
-    . . . . . . . 3 . . . . . . . . 
-    . . . . . . . 3 3 3 . . . . . . 
-    . . . . . . . 3 3 3 . . . . . . 
-    . . . . . . 3 3 . . 3 . . . . . 
-    . . . . 3 3 . 3 . . . 3 . . . . 
-    . . . 3 . . . 3 . . . . 3 . . . 
-    . . . . . . . 3 . . . . . 3 3 . 
-    . . . . . . . 3 . . . . . . . 3 
-    . . . . . . 3 . . . . . . . . . 
-    . . . . . . 3 . . . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . f d d f . . . . . . 
+    . . . . . f d 1 1 d f . . . . . 
+    . . . . . f d 1 1 d f . . . . . 
+    . . . . f d 1 1 1 1 d f . . . . 
+    . . . . f d d d d d d f . . . . 
+    . . . f d d d d d d d d f . . . 
+    . . . . f d d d d d d f . . . . 
+    . . . . . f 2 4 5 5 f . . . . . 
+    . . . . . f 2 4 4 4 f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f 2 f f . . . . . . 
+    . . . . . . . f . . . . . . . . 
     `, SpriteKind.Player)
 Ship.setPosition(51, 104)
+Ship.setStayInScreen(true)
 controller.moveSprite(Ship, 75, 0)
+Evil2spawn()
+info.setLife(3)
+let mySprite = sprites.create(img`
+    9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+    8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+    `, SpriteKind.DeadZone)
+mySprite.setPosition(81, 120)
+game.onUpdateInterval(1000, function () {
+    if (Evil_red) {
+        projectile22 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . a 1 a . . . . . . 
+            . . . . . . . 8 1 8 . . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . a 1 1 1 a . . . . . 
+            . . . . . . a 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . 8 1 1 1 8 . . . . . 
+            . . . . . . a 8 1 8 a . . . . . 
+            . . . . . . . a a a . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, Evil_red, 0, 85)
+        projectile22.setKind(SpriteKind.Bad)
+        projectile22.setFlag(SpriteFlag.AutoDestroy, true)
+    }
+})
+game.onUpdateInterval(850, function () {
+    if (Math.percentChance(75)) {
+        if (Math.percentChance(20)) {
+            Evil2spawn()
+        } else {
+            Evil = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 6 . . . . . . . . 
+                . . . . . 6 6 6 6 6 . . . . . . 
+                . . . . 6 9 9 6 9 9 6 . . . . . 
+                . . . . 6 7 9 6 9 7 6 . . . . . 
+                . . . 6 6 7 7 . 7 7 6 6 . . . . 
+                . . . 9 7 6 . . . 6 7 9 . . . . 
+                . . . 7 6 . . . . . 6 7 . . . . 
+                . . . 7 . . . . . . . 7 . . . . 
+                . . . 9 7 . . . . . 7 9 . . . . 
+                . . . . 9 . . . . . 9 . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.Enemy)
+            Evil.setPosition(randint(0, scene.screenWidth()), 0)
+            Evil.setVelocity(randint(-5, 5), 10)
+            Evil.setBounceOnWall(true)
+        }
+    }
+})
 forever(function () {
     if (sheild_true) {
         shield.setVelocity(Ship.vx, 0)
